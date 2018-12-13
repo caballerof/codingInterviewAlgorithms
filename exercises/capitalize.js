b5956dc87ffe64e1395e8671d2c9a04166276766
DESKTOP-I2DKL5L\CSoft-UG <caballero.csoft@gmail.com>
2018-12-13 12:47:17 -0600

    - Solutions for sentence capitalize .

--- exercises/capitalize.js
+++ exercises/capitalize.js
@@ -8,21 +8,42 @@
  * capitalize the first letter of each word in the string then
  * returns the capitalizes string.
  *
- * - `a short sentence` --> `A Short Sentence`
+ * - `a short sentence`     --> `A Short Sentence`
+ * - `look, it is working!` --> `Look, It Is Working!`
  */
 
-console.log(capitalize(`a short sentence`));
+console.log(capitalizeOther(`look, it is working!`));
 
 /**
- *
- * @param {string} str
+ * Capitalize the first letter of each word.
+ * @param {string} str `String` to uppercase the first letter.
+ * @return {string} `String` returns the capitalizes string.
  */
 function capitalize(str) {
-  let strArray;
-  str.split(` `).forEach(element => {
-    strArray = [...element];
-    strArray[0] = strArray[0].toUpperCase();
-    element = strArray.join(``);
+  let strArray = str.split(` `);
+  strArray.forEach((element, index) => {
+    strArray[index] =
+      element.substring(0, 1).toUpperCase() +
+      element.substring(1, element.length);
+    //console.log(letter.toUpperCase() + strRest);
   });
-  return str;
+  return strArray.join(` `);
 } // End capitalize
+
+/**
+ * Capitalize the first letter of each word.
+ * @param {string} str `String` to uppercase the first letter.
+ * @return {string} `String` returns the capitalizes string.
+ */
+function capitalizeOther(str) {
+  const words = [];
+  for (let word of str.split(` `)) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(` `);
+} // End capitalizeOther
+
+module.exports = {
+  capitalize,
+  capitalizeOther
+};
