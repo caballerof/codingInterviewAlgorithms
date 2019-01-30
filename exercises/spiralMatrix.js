2212a7225465dbf9466a25a7adfb3be5d4cd46c0
caballerof <caballero.csoft@gmail.com>
2019-01-29 21:45:28 -0600

    - another solution

--- exercises/spiralMatrix.js
+++ exercises/spiralMatrix.js
@@ -1,5 +1,77 @@
-console.log(spiralMatrix(6));
+'use strict';
+/**
+ * @author Reivaj Caballero
+ * @description Write a function that accepts an intenger N
+ * and returns a NxN spiral matrix.
+ *
+ * matrix(2)
+ * [[ 1, 2],
+ * [ 4, 3]]
+ *
+ * matrix(3)
+ * [ [ 1, 2, 3 ],
+ * [ 8, 9, 4 ],
+ * [ 7, 6, 5 ] ]
+ *
+ * matrix(4)
+ * [ [ 1, 2, 3, 4 ],
+ * [ 12, 13, 14, 5 ],
+ * [ 11, 16, 15, 6 ],
+ * [ 10, 9, 8, 7 ] ]
+ */
 
+//console.log(spiralMatrix(4));
+console.log(matrixO(4));
+
+/**
+ *
+ * @param {*} size
+ */
+function matrixO(size) {
+  const results = [];
+  let counter = 1;
+  let startColumn = 0;
+  let startRow = 0;
+  let endColumn = size - 1;
+  let endRow = size - 1;
+
+  for (let i = 0; i < size; i++) {
+    results.push([]);
+  }
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Top row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+    // Right column
+    for (let j = startRow; j <= endRow; j++) {
+      results[j][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+    // Bottom row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+    // star column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+  return results;
+} // End matrixO
+
+/**
+ *
+ * @param {*} size
+ */
 function spiralMatrix(size) {
   let numElements = size * size;
   const matrix = new Array(size).fill(`*`).map(() => new Array(size).fill(`*`));
@@ -64,5 +136,10 @@ function spiralMatrix(size) {
         break;
     }
   } // End for
-  console.log(matrix);
+  return matrix;
 } // End spiralMatrix
+
+module.exports = {
+  matrix,
+  matrixO
+};
