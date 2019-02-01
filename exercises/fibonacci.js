ac2cd9429f0d3079bec5b3dc3bbfbc62defb531e
DESKTOP-I2DKL5L\CSoft-UG <caballero.csoft@gmail.com>
2019-02-01 16:29:34 -0600

    - Fibonacci and factorial with memoization

--- exercises/fibonacci.js
+++ exercises/fibonacci.js
@@ -1,6 +1,15 @@
-console.log(fibonacci(100));
-console.log(fibonacciRec(10));
+/**
+ *
+ */
+const fibonacciMemoize = cacheFibonacciRec(fibonacciRecCache);
 
+console.log(fibonacci(2));
+console.log(fibonacciMemoize(2));
+
+/**
+ *
+ * @param {Number} number
+ */
 function fibonacci(number) {
   const fiboSeries = [0, 1];
   for (let x = 2; x <= number; x++) {
@@ -9,9 +18,46 @@ function fibonacci(number) {
   return fiboSeries[number];
 } // End fibonacci
 
+/**
+ *
+ * @param {Function} fn
+ */
+function cacheFibonacciRec(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+} // End cacheFibonacciRec
+
+/**
+ *
+ * @param {Number} number
+ */
+function fibonacciRecCache(number) {
+  if (number < 2) {
+    return number;
+  }
+  return fibonacciMemoize(number - 1) + fibonacciMemoize(number - 2);
+} // End fibonacciRecCache
+
+/**
+ *
+ * @param {Number} number
+ */
 function fibonacciRec(number) {
   if (number < 2) {
     return number;
   }
   return fibonacciRec(number - 1) + fibonacciRec(number - 2);
 } // End fibonacciRec
+
+module.exports = {
+  fibonacci,
+  fibonacciMemoize,
+  fibonacciRec
+};
